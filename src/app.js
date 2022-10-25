3a9a0004523b52f8f5b6bfaecacd285c3b6c251c
Rhea M. Carillo <rheamimicarillo@gmail.com>
2022-10-25 18:04:23 +0000

    added review code

--- src/app.js
+++ src/app.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import './app.scss';
+import "./app.scss";
 
 // Let's talk about using index.js and some other name in the component folder
 // There's pros and cons for each way of doing this ...
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form';
-import Results from './components/results';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Form from "./components/form";
+import Results from "./components/results";
 
-class App extends React.Component
-{
-  constructor(props)
-  {
+class App extends React.Component {
+  constructor(props) {
     super(props);
     this.state = {
       data: null,
@@ -20,35 +18,69 @@ class App extends React.Component
     };
   }
 
-  callApi = (requestParams) =>
-  {
+  callApi = (requestParams) => {
     // mock output
     const data = {
       count: 2,
       results: [
-        { name: 'fake thing 1', url: 'http://fakethings.com/1' },
-        { name: 'fake thing 2', url: 'http://fakethings.com/2' },
+        { name: "fake thing 1", url: "http://fakethings.com/1" },
+        { name: "fake thing 2", url: "http://fakethings.com/2" },
       ],
     };
     this.setState({
       data,
-      requestParams
+      requestParams,
     });
-  }
+  };
 
-  render()
-  {
+  render() {
     return (
       <React.Fragment>
         <Header />
-        <div>Request Method: { this.state.requestParams.method }</div>
-        <div>URL: { this.state.requestParams.url }</div>
-        <Form handleApiCall={ this.callApi } />
-        <Results data={ this.state.data } />
+        <div>Request Method: {this.state.requestParams.method}</div>
+        <div>URL: {this.state.requestParams.url}</div>
+        <Form handleApiCall={this.callApi} />
+        <Results data={this.state.data} />
         <Footer />
       </React.Fragment>
     );
   }
 }
 
+/*
+import { useState } from "react";
+function App() {
+  const [state, setState] = useState({
+    data: null,
+    requestParams: {},
+  });
+
+  callApi = (requestParams) => {
+    // mock output
+    const data = {
+      count: 2,
+      results: [
+        { name: "fake thing 1", url: "http://fakethings.com/1" },
+        { name: "fake thing 2", url: "http://fakethings.com/2" },
+      ],
+    };
+
+    setState({
+      data,
+      requestParams,
+    });
+  };
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Request Method: {state.requestParams.method}</div>
+      <div>URL: {state.requestParams.url}</div>
+      <Form handleApiCall={callApi} />
+      <Results data={state.data} />
+      <Footer />
+    </React.Fragment>
+  );
+}
+*/
 export default App;
