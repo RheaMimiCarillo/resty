0a0516dedab4609d9f2ab60f31e8710b8502c2da
RheaMimiCarillo <rheamimicarillo@gmail.com>
2022-10-26 04:54:38 +0000

    added use state for App

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import './app.scss';
 
@@ -15,6 +15,46 @@ import Results from './components/results';
   4. make handlers to handle submit events
   5. style page to look like a reputable search engine
 */
+
+const App = () =>
+{
+  // state => the current value of our component State
+  // setState => function that can update current state to a new state.
+
+
+  let [ data, setData ] = useState(null);
+
+  let [ requestParams, setRequestParams ] = useState({});
+
+  const callApi = (requestParams) =>
+  {
+    // mock output
+    const data = {
+      count: 2,
+      results: [
+        { name: "fake thing 1", url: "http://fakethings.com/1" },
+        { name: "fake thing 2", url: "http://fakethings.com/2" }
+      ]
+    };
+    // using the spread operator to maintain any previous state values.
+    setData({ data, ...data });
+    setRequestParams({ requestParams, ...requestParams })
+  };
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div>Request Method: { requestParams.method }</div>
+      <div>URL: { requestParams.url }</div>
+      <Form handleApiCall={ callApi } />
+      <Results data={ data } />
+      <Footer />
+    </React.Fragment>
+  );
+};
+
+
+/*
 class App extends React.Component
 {
   constructor(props)
@@ -56,5 +96,6 @@ class App extends React.Component
     );
   }
 }
+*/
 
 export default App;
